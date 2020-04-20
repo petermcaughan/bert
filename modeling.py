f7c817bb3d764ea32d85eec1ea373b719d2f0949
pmcaughan6 <pmcaughan6@gatech.edu>
2020-04-20 18:06:58 -0400

    Variable scope adjustment

--- modeling.py
+++ modeling.py
@@ -871,8 +871,8 @@ def transformer_model(input_tensor,
             kernel_initializer=create_initializer(initializer_range))
 
 
-      tf.logging.info("!!!!!!!!!!!!!!!! Including extra intermediate dense layer !!!!!!!!!!!!!!1")
-      with tf.variable_scope("intermediate"):
+      tf.logging.info("!!!!!!!!!!!!!!!! Including extra intermediate dense layer !!!!!!!!!!!!!!")
+      with tf.variable_scope("intermediate2"):
         intermediate_output2 = tf.layers.dense(
             intermediate_output1,
             intermediate_size,
